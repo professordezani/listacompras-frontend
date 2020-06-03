076037e1ba6f82d337ffcd0193bce1bc71b60dbf
Henrique Dezani <dezani@gmail.com>
2020-06-03 15:22:00 +0000

    CREATE e READ prontos.

--- src/ListaPage.js
+++ src/ListaPage.js
@@ -1,19 +1,51 @@
 import React, { useState, useMemo } from 'react';
 import Header from './Header';
 import api from './api';
-import { Table, TableRow, TableCell, Switch, Button} from '@material-ui/core';
+import { Table, 
+    TableRow, 
+    TableCell, 
+    Switch, 
+    Button, 
+    Dialog, 
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    TextField
+} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 function ListaPage() { 
 
     const [ produtos, setProdutos ] = useState([]);
+    const [ open, setOpen ] = useState(false);
 
+    const [ nome, setNome ] = useState('');
+    const [ quantidade, setQuantidade ] = useState(1);
+ 
     async function loadData() { 
-        const response = await api.get('/');
+        const response = await api.get('/'); 
         setProdutos(response.data);
     }
 
     useMemo(loadData, []);
+
+    function openDialog() { 
+        setOpen(true);
+    }
+    // const openDialog = () => setOpen(true);
+
+    function closeDialog() {
+        setOpen(false);
+    }
+
+    async function salvar() { 
+        await api.post('/', {nome, quantidade}); 
+        loadData();
+        setNome('');
+        setQuantidade(1);
+        closeDialog();
+    }
 
     return <>
         <Header/>
@@ -37,7 +69,42 @@ function ListaPage() {
             }
         </Table>
 
-        <Button variant="contained" color="primary">Adicionar</Button>
+        <Button 
+            onClick={openDialog}
+            variant="contained" 
+            color="primary">
+                Adicionar
+        </Button>
+
+        <Dialog open={open} onClose={closeDialog}>
+            <DialogTitle>Novo Produto</DialogTitle>
+            <DialogContent>
+                <DialogContentText>Preencha os dados para cadastrar um novo produto.</DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="nome"
+                    label="Nome"
+                    type="text"
+                    fullWidth
+                    value={nome}
+                    onChange={e => setNome(e.target.value)}
+                />
+                <TextField
+                    margin="dense"
+                    id="quantidade"
+                    label="Quantidade"
+                    type="number"
+                    fullWidth
+                    value={quantidade}
+                    onChange={e => setQuantidade(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={closeDialog}>Cancelar</Button>
+                <Button onClick={salvar}>Salvar</Button>
+            </DialogActions>
+        </Dialog>
     </>
 }
 
